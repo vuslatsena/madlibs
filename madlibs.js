ab1ba4a88681ead99a156566f9f1e7510f7d1e13
vuslatsena <vuslatsena23@gmail.com>
2023-05-04 16:40:33 +0200

    Completed Assignment

--- madlibs.js
+++ madlibs.js
@@ -26,9 +26,20 @@
  * There are multiple ways to do this, but you may want to use regular expressions.
  * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
  */
+
 function parseStory(rawStory) {
-  // Your code here.
-  return {}; // This line is currently wrong :)
+  const generalRegex = /(\w+\[\w+\]|\w+|\W)/g;
+  const wordsArray = rawStory.match(generalRegex).filter((item) => item.trim() !== "");
+  
+  return wordsArray.map((word) => {
+    if (word.endsWith("]")) {
+      const pos = word.slice(-3, -1);
+      const content = word.slice(0, -3);
+      return { word: content, pos: pos };
+    } else {
+      return { word: word };
+    }
+  });
 }
 
 /**
@@ -42,8 +53,61 @@ function parseStory(rawStory) {
  *
  * You'll want to use the results of parseStory() to display the story on the page.
  */
+
 getRawStory()
   .then(parseStory)
   .then((processedStory) => {
-    console.log(processedStory);
-  });
+    const container = document.querySelector(".madLibsRaw");
+    container.classList.add("container");
+    const paragraph = document.createElement("p");
+    container.appendChild(paragraph);
+
+    let storyInput = processedStory;
+    for (let index = 0; index < storyInput.length; index++) {
+      const element = storyInput[index];
+      if (element.pos) {
+        const input = document.createElement("input");
+        input.classList.add("user-input");
+        input.placeholder = element.pos;
+        paragraph.appendChild(input);
+      } else {
+        const text = document.createElement("span");
+        text.textContent = ` ${element.word} `;
+        paragraph.appendChild(text);
+      }
+    }
+
+    // create an empty story based on story.txt
+
+    const storyContainer = document.querySelector(".madLibsResult");
+    storyContainer.classList.add("container");
+    const storyParagraph = document.createElement("p");
+    storyContainer.appendChild(storyParagraph);
+    const inputArr = document.querySelectorAll(".user-input");
+
+    for (let index = 0; index < storyInput.length; index++) {
+      const element = storyInput[index];
+      if (!element.pos) {
+        const text = document.createElement("span");
+        text.textContent = ` ${element.word} `;
+        storyParagraph.appendChild(text);
+      } else {
+        const yetToBeFilled = document.createElement("span");
+        yetToBeFilled.classList.add("yet-to-be-filled");
+        yetToBeFilled.textContent = "_________";
+        storyParagraph.appendChild(yetToBeFilled);
+      }
+    }
+    const yetToBeFilledArray = document.querySelectorAll(".yet-to-be-filled");
+
+    for (let index = 0; index < inputArr.length; index++) {
+      const inputElement = inputArr[index];
+      inputElement.addEventListener("input", (e) => {
+        const spanElement = e.target.value;
+        yetToBeFilledArray[
+          index
+        ].innerHTML = `<span class="yet-to-be-filled"> ${spanElement} </span>`;
+      });
+    }
+  }
+);
